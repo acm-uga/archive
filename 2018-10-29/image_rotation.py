347e14037c2217c3a9c14a6849ee2008837ed2e0
Chris Barrick <cbarrick1@gmail.com>
2018-10-30 16:49:08 -0400

    Add and update solutions for 2018-10-29

---
+++ 2018-10-29/image_rotation.py
@@ -0,0 +1,123 @@
+# NumPy is the array library for Python.
+# It is part of SciPy (pronounced “Sigh Pie”), a Python-based ecosystem of
+# open-source software for mathematics, science, and engineering.
+# https://www.scipy.org/
+import numpy as np
+
+def rotate(im):
+    '''Rotate an image by 90 degrees clockwise.
+
+    Arguments:
+        im (2D array):
+            The image being rotated.
+
+    Returns:
+        A new 2D array which is like ``im`` but rotated.
+    '''
+    # Create a new array of zeros for the rotated image.
+    h, w = im.shape
+    out = np.zeros(shape=(w, h))
+
+    # The formula from source index to rotated index is: ``(y, x) -> (x, h-y)``.
+    # Think of it this way: swapping ``(y, x) -> (x, y)`` transposes the image,
+    # then ``(x, y) -> (x, h-y-1)`` reverses the rows.
+    def rotate_coords(y, x):
+        return (x, h-y-1)
+
+    # Populate the new image.
+    for y in range(h):
+        for x in range(w):
+            src = (y, x)
+            dst = rotate_coords(y, x)
+            out[dst] = im[src]
+
+    return out
+
+
+
+def rotate_inplace(im):
+    '''Rotate an image in-place by 90 degrees clockwise.
+
+    This is a weird algorithm:
+
+    We work in rings, starting with the elements touching the outer edge, then
+    the elements one index from the edge, then two from the edge, etc. For each
+    ring, we consider four pointers, each starting in a corner. We exchange the
+    elements uner the pointers in 90 rotation, then we step each pointer along
+    the ring.
+
+    Visually, we start with something like this:
+
+        [[ 0,  1,  2,  3],
+         [ 4,  5,  6,  7],
+         [ 8,  9, 10, 11],
+         [12, 23, 24, 15]]
+
+    For the first stage, we only consider the outer ring:
+
+        [[ 0,  1,  2,  3],
+         [ 4, __, __,  7],
+         [ 8, __, __, 11],
+         [12, 23, 24, 15]]
+
+    We start with pointers in the corners:
+
+        [[ 0, **, **,  3],
+         [**, __, __, **],
+         [**, __, __, **],
+         [12, **, **, 15]]
+
+    Rotate 90 degrees:
+
+        [[12, **, **,  0],
+         [**, __, __, **],
+         [**, __, __, **],
+         [15, **, **,  3]]
+
+    Then move our pointers and do it again:
+
+        [[**,  1, **, **],      [[**,  8, **, **],
+         [**, __, __,  7],  ->   [**, __, __,  1],
+         [ 8, __, __, **],       [24, __, __, **],
+         [**, **, 24, **]]       [**, **,  7, **]]
+
+    When the ring is done, we move inwards a level and repeat:
+
+        [[__, __, __, __],      [[__, __, __, __],
+         [__,  5,  6, __],  ->   [__,  9,  5, __],
+         [__,  9, 10, __],       [__, 10,  6, __],
+         [__, __, __, __]]       [__, __, __, __]]
+
+    Arguments:
+        im (2D array):
+            The image being rotated. Since this is an in-place rotation, the
+            array must be square.
+
+    Returns:
+        Returns ``im`` back. It is rotated in-place.
+    '''
+    # The shape of the image must be square.
+    h, w = im.shape
+    assert h == w
+
+    # The formula from source index to rotated index is: ``(y, x) -> (x, h-y)``.
+    # Think of it this way: swapping ``(y, x) -> (x, y)`` transposes the image,
+    # then ``(x, y) -> (x, h-y-1)`` reverses the rows.
+    def rotate_coords(y, x):
+        return (x, h-y-1)
+
+    # For each layer, for each rotation in the layer,
+    for y in range(h//2):
+        for x in range(y, w-1-y):
+            # Get coordinates for the 4 pointers.
+            a = (y, x)
+            b = rotate_coords(*a)
+            c = rotate_coords(*b)
+            d = rotate_coords(*c)
+
+            # Do the rotation.
+            # In other languages, this requires 4 temp variables.
+            im[a], im[b], im[c], im[d] \
+            = im[d], im[a], im[b], im[c]
+
+    return im
--- 2018-10-29/online_average.py
+++ 2018-10-29/online_average.py
@@ -5,6 +5,49 @@ def online_mean(avg, size, data):
         avg: The old mean.
         size: The old size.
         data: The new data.
+
+    Returns:
+        The updated average.
+    '''
+    if len(data) == 0:
+        return avg
+
+    old_sum = avg * size
+    new_sum = old_sum + sum(data)
+    new_size = size + len(data)
+    new_avg = new_sum / new_size
+
+    return new_avg
+
+
+
+def online_mean_stable(avg, size, data):
+    '''Extend a previously computed average with new data.
+
+    This version has better numerical stability by using Welfords method.
+
+    To explain numerical stability, consider the usual formula for a mean:
+    ``mean(data) = sum(data) / len(data)``. In this case, each element of
+    the data may be small, and thus the mean will be small, but the intermediate
+    sum could be huge if the dataset is huge. If the sum is huge, the result
+    could suffer from rounding error or overflow. Numerical stability is a
+    property of algorithms where the intermediate computations are always
+    within an acceptable neighborhood of the inputs and final result.
+
+    Welfords method maintains numerical stability by updating the average one
+    element at a time.
+
+    See:
+        https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Welford's_Online_algorithm
+        https://en.wikipedia.org/wiki/Numerical_stability#Stability_in_numerical_linear_algebra
+
+    Arguments:
+        avg: The old mean.
+        size: The old size.
+        data: The new data.
+
+    Returns:
+        The updated average.
     '''
     if len(data) == 0:
         return avg
@@ -13,4 +56,4 @@ def online_mean(avg, size, data):
     size = size + 1
     avg = avg + (next - avg) / size
 
-    return online_mean(avg, size, data[1:])
+    return online_mean_better(avg, size, data[1:])
