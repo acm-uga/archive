835f7ca1a5d4a97baad5b6e1c604b1aa53b686b1
adaveinthelife <33740680+adaveinthelife@users.noreply.github.com>
2019-01-22 17:08:52 -0500

    Create hanoi.py

---
+++ 2019-01-22/hanoi.py
@@ -0,0 +1,16 @@
+def hanoi(n, source, helper, target):
+    if n > 0:
+        # move tower of size n - 1 to helper:
+        hanoi(n - 1, source, target, helper)
+        # move disk from source peg to target peg
+        if source:
+            target.append(source.pop())
+        # move tower of size n-1 from helper to target
+        hanoi(n - 1, helper, source, target)
+        
+source = [4,3,2,1]
+target = []
+helper = []
+hanoi(len(source),source,helper,target)
+
+print source, helper, target
