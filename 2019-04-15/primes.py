a1512785d9ada63f477a570d0cef16cc2ae86f01
Chris Barrick <cbarrick1@gmail.com>
2019-04-15 17:22:01 -0400

    [2019-04-15] Add prime seive solution

---
+++ 2019-04-15/primes.py
@@ -0,0 +1,50 @@
+def is_int(n):
+    '''Returns true if `n` is an integer.
+
+    Arguments:
+        n (float or int):
+            The number
+
+    Returns:
+        bool:
+            True when ``n`` is an integer.
+    '''
+    return n == int(n)
+
+def primes(n):
+    '''Yields all primes less than or equal to ``n``.
+
+    This algorithm works by counting up from 2 to ``n`` and keeping track of
+    each prime it finds along the way. For each number ``i`` in this sequence,
+    we only need to check if one of the previously found prime numbers is a
+    factor. We don't need to check if composite numbers are factors because any
+    number can be broken into prime factors (by the fundamental theorem of
+    arithmetic).
+
+    This algorithm is known as the Sieve of Eratosthenes:
+    <https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes>
+
+    Arguments:
+        n (int):
+            A non-negative integer.
+
+    Yields:
+        int:
+            A prime number less than ``n``.
+    '''
+    if not is_int(n):
+        raise ValueError('input must be an integer')
+    if n < 0:
+        raise ValueError('input must not be negative')
+
+    # Special case for 1.
+    yield 1
+
+    prev_primes = []  # All non-one primes seen so far.
+    for i in range(2, n+1):
+        for p in prev_primes:
+            if is_int(i / p):
+                break
+        else:
+            prev_primes.append(i)
+            yield i
