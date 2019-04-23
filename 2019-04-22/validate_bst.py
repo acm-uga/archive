bfb415be5593abb30c83e1174bccfd8678377c42
Chris Barrick <cbarrick1@gmail.com>
2019-04-23 00:14:23 -0400

    [2019-04-22] Simplify the BST solution

--- 2019-04-22/validate_bst.py
+++ 2019-04-22/validate_bst.py
@@ -1,88 +1,35 @@
-class BinaryTree:
-    '''A simple binary tree.
-
-    Each node has a value and optional left and right subtrees. The value
-    ``None`` represents the empty tree.
-    '''
-
-    def __init__(self, val, left=None, right=None):
-        '''Construct a new binary tree.
-
-        Arguments:
-            val (any): The value at the root.
-            left (BinaryTree or None): The left subtree.
-            right (BinaryTree or None): The right subtree.
-        '''
-        self.val = val
-        self.left = left
-        self.right = right
-
-    def is_bst(self, min=float('-inf'), max=float('+inf')):
-        '''Test if this tree is a valid binary search tree.
-
-        Arguments:
-            min (any): All elements must be greater than or eqaul to this value.
-            max (any): All elements must be less than or eqaul to this value.
-        '''
-        if self.val < min:
-            return False
-
-        if max < self.val:
-            return False
-
-        if self.left is not None and not self.left.is_bst(min, self.val):
-            return False
-
-        if self.right is not None and not self.right.is_bst(self.val, max):
-            return False
-
-        return True
-
-    @classmethod
-    def from_sexp(cls, s):
-        '''Construct a BinaryTree from an s-expression.
-
-        A binary s-expression is either ``None`` or a list of three values.
-        ``None`` represents an empty tree. A list of three values represents a
-        binary tree such that the first element is the root value and the
-        second and third elements are s-expressions describing the left and
-        right subtrees respectively.
-
-        Arguments:
-            s (Sequence or None): The s-expression describing the tree.
-
-        Returns:
-            BinaryTree or None: The tree described by the s-expression.
-        '''
-        if s is None:
-            return None
-
-        (val, left, right) = s
-        left = cls.from_sexp(left)
-        right = cls.from_sexp(right)
-        return cls(val, left, right)
-
-
-def validate_bst(s):
-    '''Validates a binary search tree described by an s-expression.
+def is_bst(tree, min=float('-inf'), max=float('+inf')):
+    '''Check if a tree is a binary search tree.
 
     Arguments:
-        s (Sequence or None):
-            The s-expression describing the tree.
-            The nodes must be numbers.
-
-    Returns:
-        bool:
-            True when ``s`` describes a binary search tree.
+        tree:
+            The tree to check. The empty tree is represented by ``None``.
+            Otherwise the tree is a 3-tuple, ``(val, left, right)``, where
+            ``val`` is the value of the root node and ``left`` and ``right``
+            are subtrees.
+        min:
+            The minimum allowed value in the BST.
+        max:
+            The maximum allowed value in the BST.
     '''
-    tree = BinaryTree.from_sexp(s)
     if tree is None:
         return True
-    else:
-        return tree.is_bst()
+
+    (val, left, right) = tree
+
+    if val < min or max < val:
+        return False
+
+    if not is_bst(left, min, val):
+        return False
+
+    if not is_bst(right, val, max):
+        return False
+
+    return True
 
 
 if __name__ == '__main__':
-    assert validate_bst([4, [2, None, None], [6, None, None]]) == True
-    assert validate_bst([4, [6, None, None], [2, None, None]]) == False
-    assert validate_bst(None) == True
+    assert is_bst([4, [2, None, None], [6, None, None]]) == True
+    assert is_bst([4, [6, None, None], [2, None, None]]) == False
+    assert is_bst(None) == True
