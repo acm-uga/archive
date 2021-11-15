e04a1558dc0dadb5df734bf7d65fab06a79431b1
David Gonzalez <64766124+David-Gonzalez02@users.noreply.github.com>
2021-11-15 08:39:12 -0500

    Create TwoSum.java

---
+++ 09-14-2021/TwoSum.java
@@ -0,0 +1,13 @@
+public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> tracker = new HashMap<Integer, Integer>();
+        int len = nums.length;
+        for(int i = 0; i < len; i++){
+            if(tracker.containsKey(nums[i])){
+                int left = tracker.get(nums[i]);
+                return new int[]{left+1, i+1};
+            }else{
+                tracker.put(target - nums[i], i);
+            }
+        }
+        return new int[2];
+    }
