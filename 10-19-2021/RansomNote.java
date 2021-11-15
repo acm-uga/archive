6eb6ba9832db6a215c9cd97fec56346b154d09aa
David Gonzalez <64766124+David-Gonzalez02@users.noreply.github.com>
2021-11-15 09:08:17 -0500

    Create RansomNote.java

---
+++ 10-19-2021/RansomNote.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] arr = new int[26];
+        for (int i = 0; i < magazine.length(); i++) {
+            arr[magazine.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < ransomNote.length(); i++) {
+            if(--arr[ransomNote.charAt(i)-'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
