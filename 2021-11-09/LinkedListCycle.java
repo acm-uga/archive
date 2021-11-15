2b4a4c62553defcad2219952354edd7e8de31218
David Gonzalez <64766124+David-Gonzalez02@users.noreply.github.com>
2021-11-15 09:17:31 -0500

    Create LinkedListCycle.java

---
+++ 2021-11-09/LinkedListCycle.java
@@ -0,0 +1,11 @@
+public boolean hasCycle(ListNode head) {
+    if(head==null) return false;
+    ListNode walker = head;
+    ListNode runner = head;
+    while(runner.next!=null && runner.next.next!=null) {
+        walker = walker.next;
+        runner = runner.next.next;
+        if(walker==runner) return true;
+    }
+    return false;
+}
