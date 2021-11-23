1d8c8aaed79bbd53270a34b8643a1e470fbb555e
David Gonzalez <64766124+David-Gonzalez02@users.noreply.github.com>
2021-11-23 18:26:05 -0500

    Create SameTree.java

---
+++ 2021-11-23/SameTree.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+        else {
+            return(isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
+        }
+    }
+}
